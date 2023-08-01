e783d62410cf1428dd24d52ae84d639d78a6dea9
Andrusenko.Artem <andrusenkoartem2005@gmail.com>
2023-08-01 18:19:41 +0200

    New commit

---
+++ homeworks/homework9.js
@@ -0,0 +1,45 @@
+let names = [
+    {id: 1, name: 'Steven', salary: '5000'},
+    {id: 2, name: 'Neena', salary: '10000'},
+    {id: 3, name: 'John', salary: '4500'},
+    {id: 4, name: 'Rogers', salary: '3400'},
+    {id: 5, name: 'Cevin', salary: '500'},
+    {id: 6, name: 'Alshey', salary: '1210'},
+    {id: 7, name: 'Milki', salary: '110'},
+    {id: 8, name: 'Polar', salary: '120'},
+    {id: 9, name: 'Grindell', salary: '10'},
+]
+
+//1 Задание
+let oddId = 0;
+names.forEach(value => {
+    if (value.id % 2 !== 0) {
+      oddId++;
+    }
+});
+console.log(oddId);
+
+//2 Задание
+let sum = names.reduce((acumm, value) => {
+let i = parseInt(value.salary);
+return acumm + i}, 0); 
+console.log(sum);
+
+//3 Задание
+let namesWithE = 0;
+names.forEach(value => {
+    if (value.name.includes('e')) {
+      namesWithE++;
+    }
+});
+console.log(namesWithE);
+
+//4 Задание
+let notDivisibleBy100 = names.filter(value => parseInt(value.salary) % 100 != 0);
+console.log(notDivisibleBy100);
+
+//5 Задание
+//-
+
+//6 Задание
+//-
--- test.js
+++ test.js
@@ -1,3 +1,70 @@
+db.createCollection("workers")
+db.users.insertMany([
+    {id: 1, firstname: 'Петр', lastname: 'Сергеев', position: 'CEO', salary: 7000}
+    {id: 2, firstname: 'Виктор', lastname: 'Семенов', position: 'Web-developer', salary: 5000}
+    {id: 3, firstname: 'Никита', lastname: 'Петров', position: 'Assistant', salary: 3500}
+    {id: 4, firstname: 'Инна', lastname: 'Орлова', position: 'Accountant', salary: 4500}
+])
 
-  
-  
+db.createCollection("fruits")
+db.fruits.insertMany([
+    {
+     id: 1,
+     title: 'Apple',
+     price: 280,
+     count: 4
+    },
+    {
+    id: 2,
+    title: 'Lemon',
+    price: 300,
+    count: 8
+    },
+    {
+    id: 3,
+    title: 'Lime',
+    price: 500,
+    count: 3
+    },
+    {
+    id: 4,
+    title: 'Orange',
+    price: 200,
+    count: 8
+    },
+])
+
+gt - >
+lt - <
+gte - >=
+lte - <=
+eq - =
+ne - <>
+
+
+db.users.insertMany([
+    {
+    id: 1,
+    name: 'Анатолий',
+    age: 28,
+    gender: 'm'
+    },
+    {
+    id: 2,
+    name: 'Светлана',
+    age: 25,
+    gender: 'f'
+    },
+    {
+    id: 3,
+    name: 'Никита',
+    age: 33,
+    gender: 'm'
+    },
+    {
+    id: 4,
+    name: 'Ольга',
+    age: 22,
+    gender: 'f'
+    },
+])
