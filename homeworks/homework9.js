c063e3736500324da1c083e9c02f5e32d92db373
Andrusenko.Artem <andrusenkoartem2005@gmail.com>
2023-08-01 18:32:06 +0200

    New commit

--- homeworks/homework9.js
+++ homeworks/homework9.js
@@ -10,6 +10,7 @@ let names = [
     {id: 9, name: 'Grindell', salary: '10'},
 ]
 
+
 //1 Задание
 let oddId = 0;
 names.forEach(value => {
@@ -19,11 +20,13 @@ names.forEach(value => {
 });
 console.log(oddId);
 
+
 //2 Задание
 let sum = names.reduce((acumm, value) => {
 let i = parseInt(value.salary);
 return acumm + i}, 0); 
 console.log(sum);
+
 
 //3 Задание
 let namesWithE = 0;
@@ -34,12 +37,19 @@ names.forEach(value => {
 });
 console.log(namesWithE);
 
+
 //4 Задание
 let notDivisibleBy100 = names.filter(value => parseInt(value.salary) % 100 != 0);
 console.log(notDivisibleBy100);
 
+
 //5 Задание
-//-
+let result = names.filter(value => Number.isInteger(Math.sqrt(value.id)));
+console.log(result);
+
+//Number.isInteger проверяет, является ли переданное значение целым числом 
+
 
 //6 Задание
-//-
+let nameLength = names.filter(value => Number.isInteger(Math.sqrt(value.name.length)));
+console.log(nameLength);
